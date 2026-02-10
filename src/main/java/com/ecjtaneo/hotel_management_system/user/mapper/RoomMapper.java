28d12cdcee97523471e1234b2add85f968414cb8
EcjTn <ecjtaneo@gmail.com>
2026-02-10 14:39:40 +0800

    added mapper for rooms

---
+++ src/main/java/com/ecjtaneo/hotel_management_system/user/mapper/RoomMapper.java
@@ -0,0 +1,15 @@
+package com.ecjtaneo.hotel_management_system.user.mapper;
+
+import com.ecjtaneo.hotel_management_system.room.dto.RoomCreationDto;
+import com.ecjtaneo.hotel_management_system.room.dto.RoomPublicResponseDto;
+import com.ecjtaneo.hotel_management_system.room.model.Room;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface RoomMapper {
+    public RoomPublicResponseDto toDtoSingle(Room room);
+    public List<RoomPublicResponseDto> toDtoList(List<Room> room);
+    public Room toEntity(RoomCreationDto roomCreationDto);
+}
