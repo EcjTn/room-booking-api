ccb22eb176ff7e34e59b30be71e0f2f645f83898
EcjTn <ecjtaneo@gmail.com>
2026-02-24 21:45:08 +0800

    booking creation validation

--- src/main/java/com/ecjtaneo/hotel_management_system/booking/dto/BookingCreationDto.java
+++ src/main/java/com/ecjtaneo/hotel_management_system/booking/dto/BookingCreationDto.java
@@ -1,10 +1,14 @@
 package com.ecjtaneo.hotel_management_system.booking.dto;
 
-import java.math.BigDecimal;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record BookingCreationDto(
+        @NotNull
         String roomNumber,
+        @NotNull
         LocalDate startDate,
+        @NotNull
         LocalDate endDate
 ) {}
