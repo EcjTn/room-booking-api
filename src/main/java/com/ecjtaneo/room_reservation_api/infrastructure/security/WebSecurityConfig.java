f5c56dc0361ff9b0872f7487c35f584b54ce1078
EcjTn <ecjtaneo@gmail.com>
2026-04-16 17:17:51 +0800

    switched to httpbasic lamdba

--- src/main/java/com/ecjtaneo/room_reservation_api/infrastructure/security/WebSecurityConfig.java
+++ src/main/java/com/ecjtaneo/room_reservation_api/infrastructure/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,7 +31,7 @@ public class WebSecurityConfig {
                         .failureHandler((req, res, auth) -> {res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);})
                 )
 
-                .httpBasic(h -> h.disable())
+                .httpBasic(AbstractHttpConfigurer::disable)
 
                 .logout(l -> l
                         .logoutUrl("/auth/logout")
